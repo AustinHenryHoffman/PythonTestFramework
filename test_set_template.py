fde694cf80d6b8765b3199adac84c7441583ef03
DrTautology <tauttechsystems@gmail.com>
2020-09-29 17:12:44 -0500

    Various bug fixes

--- test_set_template.py
+++ test_set_template.py
@@ -43,62 +43,62 @@ class first_test_set(test_case):
             self.fail()
 
 
-def test_random_1_2(self):
-    """Example Test 2. Uses assertTrue method to show inequality of two numbers. This test fails."""
-    self.logger.info("Running test: " + str(self._testMethodName))
-    #self.logger.info("This number is used on all first_set test using the test_case class: " + str(self.num))
-    self.logger.info("This number is generated separately for each test: " + str(self.ran))
-    self.logger.info("This is the number used on only the first_set tests " + str(self.num2))
-    try:
-        self.assertTrue(1 == 2)
-        self.logger.info(str(self._testMethodName) + " PASSED")
-        self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_2.__doc__)]
-    except AssertionError:
-        self.logger.error(str(self._testMethodName) + " FAILED")
-        self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_2.__doc__)]
-        self.fail()
+    def test_random_1_2(self):
+        """Example Test 2. Uses assertTrue method to show inequality of two numbers. This test fails."""
+        self.logger.info("Running test: " + str(self._testMethodName))
+        #self.logger.info("This number is used on all first_set test using the test_case class: " + str(self.num))
+        self.logger.info("This number is generated separately for each test: " + str(self.ran))
+        self.logger.info("This is the number used on only the first_set tests " + str(self.num2))
+        try:
+            self.assertTrue(1 == 2)
+            self.logger.info(str(self._testMethodName) + " PASSED")
+            self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_2.__doc__)]
+        except AssertionError:
+            self.logger.error(str(self._testMethodName) + " FAILED")
+            self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_2.__doc__)]
+            self.fail()
 
 
-def test_random_1_3(self):
-    """Example Test 3. THis test shows an example of using subtests. A single test can have multiple subtests that can either pass or fail independently. In this case we are checking the range of number 1-6 to see if they are even or odd. If the number is even the test passes"""
-    self.logger.info("Running test: " + str(self._testMethodName))
-    failure = 0
-    for i in range(1, 7):
-        with self.subTest(msg="i=" + str(i)):
-            try:
-                self.assertEqual(i % 2, 0)
-                self.logger.info(str(self._testMethodName) + " iteration: " + str(i) + " PASSED")
-                self.test_cases[str(self._testMethodName) + "_" + str(i)] = ["PASSED", str(self.test_random_1_3.__doc__)]
-            except AssertionError:
-                self.logger.error(str(self._testMethodName) + " iteration: " + str(i) + " FAILED")
-                self.test_cases[str(self._testMethodName) + "_" + str(i)] = ["FAILED", str(self.test_random_1_3.__doc__)]
-                failure = 1
-    if failure == 1:
-        self.logger.error(str(self._testMethodName) + " FAILED")
-        self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_3.__doc__)]
-        self.fail()
+    def test_random_1_3(self):
+        """Example Test 3. THis test shows an example of using subtests. A single test can have multiple subtests that can either pass or fail independently. In this case we are checking the range of number 1-6 to see if they are even or odd. If the number is even the test passes"""
+        self.logger.info("Running test: " + str(self._testMethodName))
+        failure = 0
+        for i in range(1, 7):
+            with self.subTest(msg="i=" + str(i)):
+                try:
+                    self.assertEqual(i % 2, 0)
+                    self.logger.info(str(self._testMethodName) + " iteration: " + str(i) + " PASSED")
+                    self.test_cases[str(self._testMethodName) + "_" + str(i)] = ["PASSED", str(self.test_random_1_3.__doc__)]
+                except AssertionError:
+                    self.logger.error(str(self._testMethodName) + " iteration: " + str(i) + " FAILED")
+                    self.test_cases[str(self._testMethodName) + "_" + str(i)] = ["FAILED", str(self.test_random_1_3.__doc__)]
+                    failure = 1
+        if failure == 1:
+            self.logger.error(str(self._testMethodName) + " FAILED")
+            self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_3.__doc__)]
+            self.fail()
 
 
-def test_random_1_4(self):
-    """Example Test 4. This test shows an example of the assertIsNot method."""
-    try:
-        self.assertIsNot(1, 2)
-        self.logger.info(str(self._testMethodName) + " PASSED")
-        self.test_cases[str(self._testMethodName)] = ["PASSED", str(self.test_random_1_4.__doc__)]
-    except AssertionError:
-        self.logger.error(str(self._testMethodName) + " FAILED")
-        self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_4.__doc__)]
+    def test_random_1_4(self):
+        """Example Test 4. This test shows an example of the assertIsNot method."""
+        try:
+            self.assertIsNot(1, 2)
+            self.logger.info(str(self._testMethodName) + " PASSED")
+            self.test_cases[str(self._testMethodName)] = ["PASSED", str(self.test_random_1_4.__doc__)]
+        except AssertionError:
+            self.logger.error(str(self._testMethodName) + " FAILED")
+            self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_4.__doc__)]
 
 
-def test_random_1_5(self):
-    """Example test 5 showing use of assertIn method"""
-    try:
-        self.assertIn(1, [1, 2, 3])
-        self.logger.info(str(self._testMethodName) + " PASSED")
-        self.test_cases[str(self._testMethodName)] = ["PASSED", str(self.test_random_1_5.__doc__)]
-    except AssertionError:
-        self.logger.info(str(self._testMethodName) + " FAILED")
-        self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_5.__doc__)]
+    def test_random_1_5(self):
+        """Example test 5 showing use of assertIn method"""
+        try:
+            self.assertIn(1, [1, 2, 3])
+            self.logger.info(str(self._testMethodName) + " PASSED")
+            self.test_cases[str(self._testMethodName)] = ["PASSED", str(self.test_random_1_5.__doc__)]
+        except AssertionError:
+            self.logger.info(str(self._testMethodName) + " FAILED")
+            self.test_cases[str(self._testMethodName)] = ["FAILED", str(self.test_random_1_5.__doc__)]
 
 
 def logResults(result):
